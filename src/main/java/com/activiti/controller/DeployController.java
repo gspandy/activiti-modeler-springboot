ed41571864b5ab53b4385197af097214227d9461
merrywxf <352004760@qq.com>
2017-05-08 22:35:42 +0800

    Update DeployController.java

--- src/main/java/com/activiti/controller/DeployController.java
+++ src/main/java/com/activiti/controller/DeployController.java
@@ -5,18 +5,6 @@ import java.util.List;
 import org.activiti.bpmn.converter.BpmnXMLConverter;
 import org.activiti.bpmn.model.BpmnModel;
 
-/* Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import org.activiti.editor.constants.ModelDataJsonConstants;
 import org.activiti.editor.language.json.converter.BpmnJsonConverter;
@@ -33,9 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-/**
- * @author Tijs Rademakers
- */
 @RestController
 @RequestMapping("/deploy")
 public class DeployController implements ModelDataJsonConstants {
