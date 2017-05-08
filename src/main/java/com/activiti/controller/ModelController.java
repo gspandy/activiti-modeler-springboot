3d16d5f0165c5c9b9528c488565c4dbcb1c7809d
merrywxf <352004760@qq.com>
2017-05-08 22:36:10 +0800

    Update ModelController.java

--- src/main/java/com/activiti/controller/ModelController.java
+++ src/main/java/com/activiti/controller/ModelController.java
@@ -13,18 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.activiti.bpmn.converter.BpmnXMLConverter;
 import org.activiti.bpmn.model.BpmnModel;
 
-/* Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import org.activiti.editor.constants.ModelDataJsonConstants;
 import org.activiti.editor.language.json.converter.BpmnJsonConverter;
@@ -55,9 +43,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.vaadin.terminal.StreamResource.StreamSource;
 
-/**
- * @author Tijs Rademakers
- */
+
 @RestController
 @RequestMapping("/model")
 public class ModelController implements ModelDataJsonConstants {
