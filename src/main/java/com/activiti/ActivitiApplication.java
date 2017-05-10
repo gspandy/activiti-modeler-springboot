6039112a58634c9d7ccc98fae245b8c6863b043a
merrywxf <352004760@qq.com>
2017-05-10 18:12:25 +0800

    login

--- src/main/java/com/activiti/ActivitiApplication.java
+++ src/main/java/com/activiti/ActivitiApplication.java
@@ -1,9 +1,15 @@
 package com.activiti;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.activiti.interceptor.LoginFilter;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
@@ -13,5 +19,29 @@ public class ActivitiApplication {
 		SpringApplication.run(ActivitiApplication.class, args);
 	}
 
-	
+	/**
+	 * 配置过滤器
+	 * 
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean someFilterRegistration() {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(LoginFilter());
+		registration.addUrlPatterns("/*");
+		registration.addInitParameter("paramName", "paramValue");
+		registration.setName("loginFilter");
+		return registration;
+	}
+
+	/**
+	 * 创建一个bean
+	 * 
+	 * @return
+	 */
+	@Bean(name = "loginFilter")
+	public Filter LoginFilter() {
+		return new LoginFilter();
+	}
+
 }
--- src/main/java/com/activiti/controller/DeployController.java
+++ src/main/java/com/activiti/controller/DeployController.java
@@ -1,9 +1,15 @@
 package com.activiti.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
 import java.util.List;
-import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.activiti.bpmn.converter.BpmnXMLConverter;
 import org.activiti.bpmn.model.BpmnModel;
@@ -16,6 +22,7 @@ import org.activiti.engine.repository.ProcessDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,8 +37,6 @@ public class DeployController implements ModelDataJsonConstants {
 
 	@Autowired
 	private RepositoryService repositoryService;
-	@Autowired
-	private ObjectMapper objectMapper;
 
 	/**
 	 * 根据model id部署
@@ -61,14 +66,75 @@ public class DeployController implements ModelDataJsonConstants {
 		List<ProcessDefinition> list2 = this.repositoryService.createProcessDefinitionQuery().list();
 		StringBuffer sb = new StringBuffer();
 		sb.append("[");
-		list2.forEach(m -> {
-			sb.append("{id:" + m.getId() + ",deploymentId:" + m.getDeploymentId())
-					.append(",key:" + m.getKey() + ",name:" + m.getName())
-					.append(",resourceName:" + m.getResourceName())//
-					.append(",diagramResourceName:" + m.getDiagramResourceName());
-			sb.append("}");
-		});
+		for (int i = 0; i < list2.size(); i++) {
+			ProcessDefinition m = list2.get(i);
+			sb.append("{\"id\":\"" + m.getId() + "\",\"deploymentId\":\"" + m.getDeploymentId())
+					.append("\",\"key\":\"" + m.getKey() + "\",\"name\":\"" + m.getName())
+					.append("\",\"resourceName\":\"" + m.getResourceName())//
+					.append("\",\"diagramResourceName\":\"" + m.getDiagramResourceName())
+					.append("\",\"version\":\"" + m.getVersion());
+			sb.append("\"}");
+			if (i < list2.size() - 1) {
+				sb.append(",");
+			}
+		}
+
 		sb.append("]");
 		return sb.toString();
+	}
+
+	@RequestMapping("/delete/{deploymentId}")
+	public Object deleteDeploy(@PathVariable String deploymentId) {
+		this.repositoryService.deleteDeployment(deploymentId, true);
+		return true;
+	}
+
+	@RequestMapping("/showDeploySource")
+	public Object showDeploySource(String deploymentId, String resourceName, HttpServletResponse response) {
+		InputStream is = this.repositoryService.getResourceAsStream(deploymentId, resourceName);
+		String ss = "slkdfjlaskdfj";
+		ByteArrayInputStream bai = new ByteArrayInputStream(ss.getBytes());
+		Reader reader = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(reader);// 缓冲流
+		String str = null;
+		StringBuffer sb = new StringBuffer();
+		try {
+			while ((str = br.readLine()) != null) {
+				sb.append(str);
+			}
+			System.out.println(sb.toString());
+			br.close();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return sb.toString();
+	}
+
+	@RequestMapping("/showImage")
+	public Object showImage(String deploymentId, String diagramResourceName, HttpServletResponse response) {
+		InputStream is = this.repositoryService.getResourceAsStream(deploymentId, diagramResourceName);
+		response.setContentType("application/x-msdownload;charset=UTF-8");
+		response.reset();// 清除缓冲中的数据
+		int temp;
+		OutputStream os = null;
+		try {
+			os = response.getOutputStream();
+			while ((temp = is.read()) != (-1)) {
+				os.write(temp);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+				os.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return null;
 	}
 }
